54de141128c9af02c3625476afde44519bd7d494
Marcos <59671002+Knamer95@users.noreply.github.com>
2022-09-23 22:18:37 +0200

    feature: Improved the contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,11 +1,13 @@
 import { memo } from 'react';
-import { Anchor, Box, Group, Space, Text } from '@mantine/core';
+import { Box, Center, Grid, Group, Image, Space, Text } from '@mantine/core';
 import { useDocumentTitle } from '@mantine/hooks';
 import { Trans } from 'react-i18next';
 
+import ContactMeImage from '@/assets/misc/contact-scaled.png';
 import { SocialLinks } from '@/components/common';
 import { DokaButton, DokaCard, DokaCardBody, DokaCardHeader } from '@/components/mantine';
 import { useTranslate } from '@/hooks';
+import { DokaAnchor } from '@/components/mantine/DokaAnchor';
 
 const Contact = memo(() => {
   const t = useTranslate('contact');
@@ -16,68 +18,97 @@ const Contact = memo(() => {
       color: 'pink' as const,
       variant: 'light' as const,
       compact: true,
-      component: 'a',
+      component: 'a' as const,
       target: '_blank',
+    },
+    anchor: {
+      color: 'pink' as const,
+    },
+    image: {
+      styles: {
+        //
+      },
     },
   };
 
   return (
     <>
-      <DokaCard>
-        <DokaCardHeader>{t('title')}</DokaCardHeader>
-        <DokaCardBody>
-          <Box sx={{ maxWidth: 1000 }}>
-            <Text>{t('p1')}</Text>
+      <Grid>
+        <Grid.Col span={7}>
+          <DokaCard>
+            <DokaCardHeader>{t('title')}</DokaCardHeader>
+            <DokaCardBody>
+              <Box>
+                <Text>{t('p1')}</Text>
 
-            <Space h={30} />
+                <Space h={30} />
 
-            <Text>
-              <Trans
-                components={{
-                  RepoAnchor: <Anchor href="https://github.com/Knamer95/react-knamer-web" />,
-                }}
-              >
-                {t('p2')}
-              </Trans>
-            </Text>
+                <Text>{t('p2')}</Text>
 
-            <Text>
-              <Trans
-                components={{
-                  ReactAnchor: (
-                    <DokaButton href="https://beta.reactjs.org/" {...childrenProps.button} />
-                  ),
-                  TypeScriptAnchor: (
-                    <DokaButton href="https://www.typescriptlang.org/" {...childrenProps.button} />
-                  ),
-                  ViteAnchor: <DokaButton href="https://vitejs.dev/" {...childrenProps.button} />,
-                  MantineAnchor: (
-                    <DokaButton href="https://mantine.dev/" {...childrenProps.button} />
-                  ),
-                }}
-              >
-                {t('p3')}
-              </Trans>
-            </Text>
+                <Space h={30} />
 
-            <Space h={30} />
+                <Text>
+                  <Trans
+                    components={{
+                      RepoAnchor: (
+                        <DokaAnchor
+                          href="https://github.com/Knamer95/react-knamer-web"
+                          {...childrenProps.anchor}
+                        />
+                      ),
+                    }}
+                  >
+                    {t('p3')}
+                  </Trans>
+                </Text>
 
-            <Text>{t('p4')}</Text>
-          </Box>
+                <Text>
+                  <Trans
+                    components={{
+                      ReactAnchor: (
+                        <DokaButton href="https://beta.reactjs.org/" {...childrenProps.button} />
+                      ),
+                      TypeScriptAnchor: (
+                        <DokaButton
+                          href="https://www.typescriptlang.org/"
+                          {...childrenProps.button}
+                        />
+                      ),
+                      ViteAnchor: (
+                        <DokaButton href="https://vitejs.dev/" {...childrenProps.button} />
+                      ),
+                      MantineAnchor: (
+                        <DokaButton href="https://mantine.dev/" {...childrenProps.button} />
+                      ),
+                    }}
+                  >
+                    {t('p4')}
+                  </Trans>
+                </Text>
+              </Box>
 
-          <Space h={30} />
+              <Space h={30} />
 
-          <Group spacing="xs" sx={{ fontSize: 20 }}>
-            <SocialLinks />
-          </Group>
-          
-          {/* <Space h={30} />
+              <Group spacing="xs" sx={{ fontSize: 20 }}>
+                <SocialLinks />
+              </Group>
+
+              {/* <Space h={30} />
 
           {Array.from({ length: 40 }).map((_, index) => (
             <Box key={`fill-text-${index}`}>Fill text to remove</Box>
           ))} */}
-        </DokaCardBody>
-      </DokaCard>
+            </DokaCardBody>
+          </DokaCard>
+        </Grid.Col>
+        <Grid.Col span={5}>
+          <DokaCard>
+            <Box sx={{ position: 'relative' }}>
+              <Image src={ContactMeImage} alt="Contact me image" {...childrenProps.image} />
+            </Box>
+          </DokaCard>
+        </Grid.Col>
+      </Grid>
     </>
   );
 });
