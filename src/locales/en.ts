c57744d3b32aabae7b426fa9e3ab394c3b5a93c9
Marcos <59671002+Knamer95@users.noreply.github.com>
2022-09-23 22:17:20 +0200

    Added/edited language packages

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -11,13 +11,13 @@ export default {
   contact: {
     title: 'Contact me',
     htmlTitle: 'Contact me',
-    p1: `Currently I'm not interested in any kind of freelance work, nor open to job offers, but I enjoy making
-    projects/contributions on my spare time for the sake of learning and growing up as a
-    developer.`,
-    p2: 'If you are interested in the technologies used in this webpage, you can find them in the <RepoAnchor>repo</RepoAnchor>.',
-    p3: `Mainly built with: <ReactAnchor>React 18</ReactAnchor> with <TypeScriptAnchor>TypeScript</TypeScriptAnchor>, 
+    p1: `You can contact me at any of the social links found below, or at the footer. Whether it's to say hi, ask me a question, propose some kind of collab, or talk 
+    about programming and stuff, anything is fine! I really love talking about coding, specially JS/TS and React though. :)`,
+    p2: `Currently I'm doing what I love, and I'm having a great time, so I'm not interested in any kind of freelance work, nor open to job offers, but I enjoy making
+    projects/contributions on my spare time for the sake of learning and growing up as a developer.`,
+    p3: 'If you are interested in the technologies used in this webpage, you can find them in the <RepoAnchor>repo</RepoAnchor>.',
+    p4: `Mainly built with: <ReactAnchor>React 18</ReactAnchor> with <TypeScriptAnchor>TypeScript</TypeScriptAnchor>, 
     <ViteAnchor>Vite</ViteAnchor>, and <MantineAnchor>Mantine 5</MantineAnchor>.`,
-    p4: `If you'd like to collaborate with me, ask me anything, or just want to hang out, feel free to contact me through any of my social plaforms below.`,
   },
   navbar: {
     home: 'Home',
@@ -33,6 +33,7 @@ export default {
   notFound: {
     title: 'Page not found',
     htmlTitle: 'Error 404',
-    body: 'The requested page was not found. Please check that the URL exists.',
+    body: 'Oh noes! The requested page could not be found. Please check that the URL exists.',
+    takeMeHome: 'Take me home',
   },
 };
--- src/locales/es.ts
+++ src/locales/es.ts
@@ -11,13 +11,13 @@ export default {
   contact: {
     title: 'Contacto',
     htmlTitle: 'Contacto',
-    p1: `Actualmente no estoy interesado en ningún tipo de trabajo freelance, ni abierto a nuevas ofertas de trabajo, pero me gusta hacer
-    proyectos/contribuciones en mi tiempo libre para aprender, y crecer como desarrollador.`,
-    p2: 'Si estás interesado en las tecnologías usadas, puedes ver el código de la web en el <RepoAnchor>repo</RepoAnchor>.',
-    p3: `Principalmente construido con: <ReactAnchor>React 18</ReactAnchor> con <TypeScriptAnchor>TypeScript</TypeScriptAnchor>, 
+    p1: `Puedes contactarme en cualquiera de los links que se encuentran abajo, o en el pie. Ya sea para decir hola, preguntarme algo, proponer algún tipo de colab, 
+    o hablar de programación y otras cosas, ¡cualquier cosa está bien! Me encanta hablar de programación, especialmente JS/TS y React. :)`,
+    p2: `Actualmente me encuentro haciendo lo que me gusta, y estoy pasándomelo genial, por lo que no estoy interesado en ningún tipo de trabajo freelance, ni abierto 
+    a nuevas ofertas de trabajo, pero me gusta hacer proyectos/contribuciones en mi tiempo libre para aprender, y crecer como desarrollador.`,
+    p3: 'Si estás interesado en las tecnologías usadas, puedes ver el código de la web en el <RepoAnchor>repo</RepoAnchor>.',
+    p4: `Principalmente construido con: <ReactAnchor>React 18</ReactAnchor> con <TypeScriptAnchor>TypeScript</TypeScriptAnchor>, 
     <ViteAnchor>Vite</ViteAnchor>, y <MantineAnchor>Mantine 5</MantineAnchor>.`,
-    p4: `Si te gustaría colaborar conmigo, preguntarme algo, o simplemente tener una charla, siéntete libre de contactarme a través de 
-    mis plataformas sociales.`,
   },
   navbar: {
     home: 'Home',
@@ -33,6 +33,7 @@ export default {
   notFound: {
     title: 'Página no encontrada',
     htmlTitle: 'Error 404',
-    body: 'La página solicitada no se ha encontrado. Por favor comprueba que la URL existe.',
+    body: '¡Oh no! La página a la que intentas acceder no se ha encontrado. Por favor comprueba que la URL existe.',
+    takeMeHome: 'Llévame a la home',
   },
 };
