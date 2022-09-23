f137ffc110c74fb453f13c9daa0fd6f9d1f100c7
Marcos <59671002+Knamer95@users.noreply.github.com>
2022-09-23 22:18:22 +0200

    feature: Improved the not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,24 +1,55 @@
 import { memo } from 'react';
-import { Box, Text } from '@mantine/core';
+import { BackgroundImage, Box, Grid, Image, Space, Text } from '@mantine/core';
 import { useDocumentTitle } from '@mantine/hooks';
 
-import { DokaCard, DokaCardBody, DokaCardHeader } from '@/components/mantine';
+import NotFoundImage from '@/assets/misc/not-found.png';
+import { DokaButton, DokaCard, DokaCardBody, DokaCardHeader } from '@/components/mantine';
 import { useTranslate } from '@/hooks';
 
 const NotFound = memo(() => {
   const t = useTranslate('notFound');
   useDocumentTitle(t('title'));
 
+  const childrenProps = {
+    button: {
+      color: 'pink' as const,
+      variant: 'light' as const,
+      component: 'a' as const,
+      target: '_blank',
+    },
+    image: {
+      styles: {
+        image: {
+          padding: '0 10% 10% 10%',
+        },
+      },
+    },
+  };
+
   return (
     <>
-      <DokaCard>
-        <DokaCardHeader>{t('title')}</DokaCardHeader>
-        <DokaCardBody>
-          <Box>
-            <Text>{t('body')}&nbsp;</Text>
-          </Box>
-        </DokaCardBody>
-      </DokaCard>
+      <Grid>
+        <Grid.Col span={8}>
+          <DokaCard>
+            <DokaCardHeader>{t('title')}</DokaCardHeader>
+            <DokaCardBody>
+              <Box>
+                <Text>{t('body')}&nbsp;</Text>
+                <Space h={20} />
+                <DokaButton href="/home" {...childrenProps.button}>
+                  {t('takeMeHome')}
+                </DokaButton>
+              </Box>
+            </DokaCardBody>
+          </DokaCard>
+        </Grid.Col>
+
+        <Grid.Col span={4}>
+          <DokaCard>
+            <Image src={NotFoundImage} {...childrenProps.image} />
+          </DokaCard>
+        </Grid.Col>
+      </Grid>
     </>
   );
 });
